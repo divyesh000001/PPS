8b3b800d8c5be40dcd1fe26f72b1ee6306fcf8ff
divyesh000001 <prajapati.divyesh2400@gmail.com>
2026-02-03 20:54:21 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,41 @@
+  GNU nano 7.2                                                                p1.c
+#include<stdio.h>
+int main(void)
+{
+      printf("********Arithimatic Operation*********");
+      int x,h;
+      printf("enter the value of a & b:\n");
+      scanf("%d %d",&x,&h);
+      printf("add is:%d\n",x+h);
+      printf("sub is:%d\n",x-h);
+      printf("multi is:%d\n",x*h);
+      printf("div is:%d\n",x/h);
+      printf("rem is:%d\n",x%h);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                     [ Read 13 lines ]
+^G Help          ^O Write Out     ^W Where Is      ^K Cut           ^T Execute       ^C Location      M-U Undo         M-A Set Mark     M-] To Bracket
+^X Exit          ^R Read File     ^\ Replace       ^U Paste         ^J Justify       ^/ Go To Line    M-E Redo         M-6 Copy         ^Q Where Was
